3becb57a56ca460e669a9d150d397410d1a8524d
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-08-01 16:04:56 -0400

    done

---
+++ lib/migrations/versions/c87f14c0b08e_empity_init.py
@@ -0,0 +1,24 @@
+"""Empity Init
+
+Revision ID: c87f14c0b08e
+Revises: 
+Create Date: 2023-08-01 12:50:55.418272
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c87f14c0b08e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
